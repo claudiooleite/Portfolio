781989e93884aa376deec594b0e05e4c63763168
claudiooleite <152962012+claudiooleite@users.noreply.github.com>
2024-04-23 09:35:04 +0200

    card component styled

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,7 +1,8 @@
-import { Heading, HStack, Image, Text, VStack, Card, CardHeader, CardBody, CardFooter } from "@chakra-ui/react";
+import { Heading, HStack, Image, Text, Stack, Card, CardHeader, CardBody, CardFooter } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
+
 
 const Cards = ({ title, description, imageSrc }) => {
   // Implement the UI for the Card component according to the instructions.
@@ -10,13 +11,20 @@ const Cards = ({ title, description, imageSrc }) => {
   return (
     <>
       <Card>
+         
         <CardBody>
-          <Image src={imageSrc}/>
-          <VStack>
-            <Heading>{title}</Heading>
+          <Image src={imageSrc} borderRadius='lg'/>
+          <Stack mt='6' spacing='3'>
+            <Heading  as='h3' size='lg'>{title}</Heading>
             <Text>{description}</Text>
-          </VStack>
+          </Stack>
         </CardBody>
+        <CardFooter>
+          <HStack>
+            <Heading size='xs'>See More</Heading>
+            <FontAwesomeIcon icon={faArrowRight} size="1x"/>
+          </HStack>
+        </CardFooter>
       </Card>
 
     </>
