8d5dd45ad1007158c6a514e5bfe2f2ead7893d34
claudiooleite <152962012+claudiooleite@users.noreply.github.com>
2024-04-28 12:42:26 +0200

    'names updated'

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -7,30 +7,27 @@ import React from "react";
 const Cards = ({ title, description, imageSrc, link}) => {
 
   return (
-
-    <a  
-      href={link} 
-      target="_blank" 
-      rel="noopener noreferrer"
-    >
-      <Card >
-        <CardBody >
-          <Image src={imageSrc} borderRadius='lg' alt={title}/>
-          <Stack mt='6' spacing='3'>
-            <Heading  as='h3' size='md'>{title}</Heading>
-            <Text color="#64748b" fontSize="lg">{description}</Text>
-          </Stack>
-        </CardBody>
-        <CardFooter>
-          <HStack spacing={2} alignItems="center">
-            <p>See more</p> 
-            
-              <FontAwesomeIcon icon={faArrowRight} size="1x"/>
-            
-          </HStack>
-        </CardFooter>
-      </Card>
-    </a>
+      <a  
+        href={link} 
+        target="_blank" 
+        rel="noopener noreferrer"
+      >
+        <Card >
+          <CardBody >
+            <Image src={imageSrc} borderRadius='lg' alt={title}/>
+            <Stack mt='6' spacing='3'>
+              <Heading  as='h3' size='md'>{title}</Heading>
+              <Text color="#64748b" fontSize="lg">{description}</Text>
+            </Stack>
+          </CardBody>
+          <CardFooter>
+            <HStack spacing={2} alignItems="center">
+              <p>See more</p>     
+                <FontAwesomeIcon icon={faArrowRight} size="1x"/>
+            </HStack>
+          </CardFooter>
+        </Card>
+      </a>
   )
 };
 
