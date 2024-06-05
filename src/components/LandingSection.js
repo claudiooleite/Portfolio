587c2651aa3ddc5e75729f3f83897e5df9ceb3d8
claudiooleite <152962012+claudiooleite@users.noreply.github.com>
2024-06-05 09:19:36 +0200

    landingsection updated

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -1,139 +1,53 @@
-import React, { useEffect } from "react";
-import { useFormik } from "formik";
-import {
-  Box,
-  Button,
-  FormControl,
-  FormErrorMessage,
-  FormLabel,
-  Heading,
-  Input,
-  Select,
-  Textarea,
-  VStack,
-  useMediaQuery,
-} from "@chakra-ui/react";
-import * as Yup from "yup";
+import React from "react";
+import { Avatar, Heading, VStack, Text } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
-import useSubmit from "../hooks/useSubmit";
-import { useAlertContext } from "../context/alertContext";
+import image from '../images/claudio-linkedin.jpg'
+
+const greeting = "Nice to meet you! I'm Claudio Leite.";
+const bio =
+  "A passionate front-end developer with a knack for creating dynamic and user-friendly web applications. ";
 
 const LandingSection = () => {
-  const { isLoading, response, submit } = useSubmit();
-  const { onOpen } = useAlertContext();
-
-  const formik = useFormik({
-    initialValues: {
-      firstName: "",
-      email: "",
-      type: "hireMe",
-      comment: "",
-    },
-    onSubmit: (values) => {
-      submit(values);
-    },
-    validationSchema: Yup.object({
-      firstName: Yup.string()
-        .min(2, "Too Short!")
-        .max(50, "Too Long!")
-        .required("Required"),
-      email: Yup.string().email("Invalid email").required("Required"),
-      type: Yup.string()
-        .oneOf(["hireMe", "openSource", "other"])
-        .required("Required"),
-      comment: Yup.string().min(25, "Too Short!").required("Required"),
-    }),
-  });
-
-  useEffect(() => {
-    if (response) {
-      onOpen(response.type, response.message);
-      if (response.type === "success") {
-        formik.resetForm();
-      }
+  const handleClick = (anchor) => () => {
+    const id = `${anchor}-section`;
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
     }
-  }, [response]);
-
-  const [isLargerThan768] = useMediaQuery("(min-width: 768px)");
-  const [isLargerThan1028] = useMediaQuery("(min-width: 1028px)");
+  };
   return (
     <FullScreenSection
+      justifyContent="center"
       isDarkBackground
-      backgroundColor="#512DA8"
-      py={16}
-      spacing={8}
+      backgroundColor="#2A4365"
     >
-      <VStack
-        w={isLargerThan1028 ? "1080px" : isLargerThan768 ? "700px" : "350px"}
-        p={32}
-        alignItems="flex-start"
-        paddingRight={isLargerThan768 ? "3.75em" : "1.25em"}
-        paddingLeft={isLargerThan768 ? "3.75em" : "1.25em"}
-      >
-        <Heading as="h1" id="contactme-section">
-          Contact me
-        </Heading>
-        <Box p={6} rounded="md" w="100%">
-          <form onSubmit={formik.handleSubmit}>
-            <VStack spacing={4}>
-              <FormControl
-                isInvalid={
-                  formik.touched.firstName && Boolean(formik.errors.firstName)
-                }
-              >
-                <FormLabel htmlFor="firstName">Name</FormLabel>
-                <Input
-                  id="firstName"
-                  type="text"
-                  {...formik.getFieldProps("firstName")}
-                />
-                <FormErrorMessage>{formik.errors.firstName}</FormErrorMessage>
-              </FormControl>
-              <FormControl
-                isInvalid={formik.touched.email && Boolean(formik.errors.email)}
-              >
-                <FormLabel htmlFor="email">Email Address</FormLabel>
-                <Input
-                  id="email"
-                  type="email"
-                  {...formik.getFieldProps("email")}
-                />
-                <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
-              </FormControl>
-              <FormControl>
-                <FormLabel htmlFor="type">Type of enquiry</FormLabel>
-                <Select id="type" type="type" {...formik.getFieldProps("type")}>
-                  <option value="hireMe">Freelance project proposal</option>
-                  <option value="openSource">
-                    Open source consultancy session
-                  </option>
-                  <option value="other">Other</option>
-                </Select>
-              </FormControl>
-              <FormControl
-                isInvalid={
-                  formik.touched.comment && Boolean(formik.errors.comment)
-                }
-              >
-                <FormLabel htmlFor="comment">Your message</FormLabel>
-                <Textarea
-                  id="comment"
-                  type="comment"
-                  {...formik.getFieldProps("comment")}
-                />
-                <FormErrorMessage>{formik.errors.comment}</FormErrorMessage>
-              </FormControl>
-              <Button
-                type="submit"
-                colorScheme="purple"
-                width="full"
-                isLoading={isLoading}
-              >
-                Submit
-              </Button>
-            </VStack>
-          </form>
-        </Box>
+      <VStack spacing={4}>
+        <VStack textAlign="center">
+          <Avatar
+            size="2xl"
+            name="Claudio"
+            src={image}
+            alt="Profile Picture"
+          />
+          <Heading as="h1" size="3xl">
+            {greeting}
+          </Heading>
+        </VStack>
+        <VStack spacing={6} textAlign="center">
+          <Heading as="h4" size="md">
+            {bio}
+          </Heading>
+        </VStack>
+        <VStack>
+          <Text as="u" fontSize="2xl">
+            <a href="/#contact-me" onClick={handleClick("contactme")}>
+              <span>Contact Me</span>
+            </a>
+          </Text>
+        </VStack>
       </VStack>
     </FullScreenSection>
   );
